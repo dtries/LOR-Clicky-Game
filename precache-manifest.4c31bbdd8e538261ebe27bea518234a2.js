56d8545d52d409a4b23829ef90d0a628e7205736
Dennis Ries <dtries@gmail.com>
2019-04-07 11:49:14 -0600

    Updates

--- precache-manifest.4c31bbdd8e538261ebe27bea518234a2.js
+++ precache-manifest.4c31bbdd8e538261ebe27bea518234a2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lor-clicky-game/static/js/runtime~main.836dfb5a.js"
   },
   {
-    "revision": "c6162a7b218a71888065",
-    "url": "/lor-clicky-game/static/js/main.ec068939.chunk.js"
+    "revision": "be08fa7fb4908f978207",
+    "url": "/lor-clicky-game/static/js/main.2e6e8a4a.chunk.js"
   },
   {
     "revision": "685d0c597e8419129599",
     "url": "/lor-clicky-game/static/js/2.0e3f6a1a.chunk.js"
   },
   {
-    "revision": "c6162a7b218a71888065",
+    "revision": "be08fa7fb4908f978207",
     "url": "/lor-clicky-game/static/css/main.d18f85f1.chunk.css"
   },
   {
-    "revision": "4e0ddd055a42c04e4df0bd3e17a16e8a",
+    "revision": "bf7f834332d3746a39622112186d81b6",
     "url": "/lor-clicky-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lor-clicky-game/precache-manifest.435b9af4ca68ba7c6f1eb089b88c5929.js"
+  "/lor-clicky-game/precache-manifest.4c31bbdd8e538261ebe27bea518234a2.js"
 );
 
 workbox.clientsClaim();
