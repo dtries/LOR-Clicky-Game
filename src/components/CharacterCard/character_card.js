9653520b6600d2d9646ae001e1ddf642d5c610c6
Dennis Ries <dtries@gmail.com>
2019-04-10 23:21:19 -0600

    media screen results better

--- src/components/CharacterCard/character_card.js
+++ src/components/CharacterCard/character_card.js
@@ -3,7 +3,6 @@ import "./style.css";
 
 function CharacterCard(props) {
 
-    // console.log(`ID is ${props.id}`);
     //This is a function (not a class) so it does not require a render
     return (
         <div className="card" onClick={props.handleClick}> 
